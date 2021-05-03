0538069d137e82f1fd697c16e0c2690ae0ceb90a
Vadim Kasatkin <chack.k@mail.ru>
2021-05-03 20:01:29 +0400

    complete first task

--- src/task1.test.js
+++ src/task1.test.js
@@ -1,3 +1,5 @@
+import { sumAndMultiply, sumDigitOfNumber, sumOfLengthStrings } from "./task1";
+
 const originalConsoleLog = console.log;
 beforeEach(() => {
   console.log = jest.fn();
@@ -8,36 +10,36 @@ afterEach(() => {
 
 describe("Console sum and product two numbers", () => {
   it("should correct values", () => {
-    // log(3, 14);
+    sumAndMultiply(3, 14);
     expect(console.log).toHaveBeenCalledWith(3 + 14, 3 * 14);
 
-    // log(-2, 3);
+    sumAndMultiply(-2, 3);
     expect(console.log).toHaveBeenCalledWith(-2 + 3, -2 * 3);
   });
 
   it("should be false", () => {
-    // log(3, "14");
+    sumAndMultiply(3, "14");
     expect(console.log).toHaveBeenCalledWith(false);
 
-    // log("sdfsdf", "23");
+    sumAndMultiply("sdfsdf", "23");
     expect(console.log).toHaveBeenCalledWith(false);
   });
 });
 
 describe("Sum of length two strings", () => {
   it("should correct values", () => {
-    // sumLengthString("wer", "df");
+    sumOfLengthStrings("wer", "df");
     expect(console.log).toHaveBeenCalledWith(5);
 
-    // sumLengthString("rertt", "dff");
+    sumOfLengthStrings("rertt", "dff");
     expect(console.log).toHaveBeenCalledWith(8);
   });
 
   it("should be false", () => {
-    // sumLengthString(234234, "14");
+    sumOfLengthStrings(234234, "14");
     expect(console.log).toHaveBeenCalledWith(false);
 
-    // sumLengthString([1, 2, 3], "23");
+    sumOfLengthStrings([1, 2, 3], "23");
     expect(console.log).toHaveBeenCalledWith(false);
   });
 });
@@ -45,21 +47,25 @@ describe("Sum of length two strings", () => {
 describe("Sum digit of number", () => {
   it("should be ok", () => {
     jest.spyOn(window, "prompt").mockReturnValue(123);
-    // sumDigitNumber();
+    sumDigitOfNumber();
     expect(console.log).toHaveBeenCalledWith(6);
 
     jest.spyOn(window, "prompt").mockReturnValue(999);
-    // sumDigitNumber();
+    sumDigitOfNumber();
     expect(console.log).toHaveBeenCalledWith(27);
   });
 
   it("should be false", () => {
     jest.spyOn(window, "prompt").mockReturnValue("23423");
-    // sumDigitNumber();
+    sumDigitOfNumber();
     expect(console.log).toHaveBeenCalledWith(false);
 
-    jest.spyOn(window, "prompt").mockReturnValue("");
-    // sumDigitNumber();
-    expect(console.log).toHaveBeenCalledWith({ a: 1, b: 2 });
+    jest.spyOn(window, "prompt").mockReturnValue(1233);
+    sumDigitOfNumber();
+    expect(console.log).toHaveBeenCalledWith(false);
+
+    jest.spyOn(window, "prompt").mockReturnValue({ a: 123, b: "sdfds" });
+    sumDigitOfNumber();
+    expect(console.log).toHaveBeenCalledWith(false);
   });
 });
