31cf552bcc2ab9b19bb3159d55bbb3b380fb5eac
Vadim Kasatkin <penguin2804@gmail.com>
2021-05-08 15:01:27 +0400

    create test for task 4

---
+++ src/task4.test.js
@@ -0,0 +1,18 @@
+import { user, admin } from "./task4";
+
+describe("Copy object", () => {
+  it("needle", () => {
+    jest.spyOn(window, "prompt").mockReturnValue(12);
+    user.setAge();
+    expect(user.age).toBe(12);
+
+    jest.spyOn(window, "prompt").mockReturnValue("sdsd");
+    user.setAge();
+    expect(user.age).toBe(0);
+
+    expect(admin).not.toBe(user);
+
+    expect(admin.role).toBe("admin");
+    expect(user.role).toBeUndefined();
+  });
+});
