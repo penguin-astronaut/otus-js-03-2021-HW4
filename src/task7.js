87ad1eea815dc292cac36b0b743d83b2312230cf
Vadim Kasatkin <penguin2804@gmail.com>
2021-05-10 16:38:27 +0400

    add function templateInit

--- src/task7.js
+++ src/task7.js
@@ -1,24 +1,39 @@
-const btn = document.querySelector(".btn");
-const input = document.querySelector(".input");
+export default function templateInit() {
+  document.body.querySelector(".app").innerHTML =
+    '<div class="form">' +
+    '  <input type="text" placeholder="input text" class="input">' +
+    '  <button class="btn" style="display: none;">add</button>' +
+    "</div>" +
+    '<div class="texts">' +
+    '  <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum distinctio sed voluptas totam accusantium. Delectus eveniet adipisci recusandae, sit quidem atque temporibus a harum nisi nam doloremque voluptate, soluta ea?</p>' +
+    '  <p class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quidem voluptatum incidunt, esse numquam adipisci consectetur quos odit consequatur voluptate excepturi aliquid earum modi dolor, culpa obcaecati, voluptates aspernatur in.</p>' +
+    '  <p class="text">no lorem</p>' +
+    "</div>";
 
-input.addEventListener("keyup", () => {
-  if (input.value.length === 0) {
-    btn.style.display = "none";
-  } else {
-    btn.style.display = "inline";
-  }
-});
+  const btn = document.querySelector(".btn");
+  const input = document.querySelector(".input");
 
-btn.addEventListener("click", () => {
-  const texts = document.querySelectorAll(".text");
+  input.addEventListener("keyup", () => {
+    if (input.value.length === 0) {
+      btn.style.display = "none";
+    } else {
+      btn.style.display = "inline";
+    }
+  });
 
-  if (texts.length > 4) {
-    texts[0].remove();
-  }
+  btn.addEventListener("click", () => {
+    const texts = document.querySelectorAll(".text");
 
-  const p = document.createElement("p");
-  p.classList.add("text");
-  p.textContent = input.value;
+    if (texts.length > 4) {
+      texts[0].remove();
+    }
 
-  document.querySelector(".texts").append(p);
-});
+    const p = document.createElement("p");
+    p.classList.add("text");
+    p.textContent = input.value;
+
+    document.querySelector(".texts").append(p);
+  });
+}
+
+templateInit();
---
+++ src/task7.test.js
@@ -0,0 +1,72 @@
+import templateInit from "./task7";
+
+describe("DOM test", () => {
+  document.body.innerHTML = '<div class="app"></div>';
+  templateInit();
+
+  it("check temaplate", () => {
+    const btn = document.querySelector(".btn");
+    const input = document.querySelector(".input");
+    const textsContainer = document.querySelector(".texts");
+    const texts = document.querySelectorAll(".text");
+
+    expect(btn).toBeTruthy();
+    expect(input).toBeTruthy();
+    expect(textsContainer).toBeTruthy();
+    expect(texts).toBeTruthy();
+    expect(texts.length).toBe(3);
+  });
+
+  it("show & hide button", () => {
+    const btn = document.querySelector(".btn");
+    const input = document.querySelector(".input");
+
+    input.value = "some text";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+
+    expect(btn.style.display).toBe("inline");
+
+    input.value = "";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+
+    expect(btn.style.display).toBe("none");
+  });
+
+  it("add new item", () => {
+    const btn = document.querySelector(".btn");
+    const input = document.querySelector(".input");
+
+    input.value = "some text";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+    btn.click();
+
+    expect(document.querySelectorAll(".text").length).toBe(4);
+    expect(document.querySelectorAll(".text")[3].innerHTML).toBe("some text");
+    document.querySelectorAll(".text")[3].remove();
+  });
+
+  it('max "p" count', () => {
+    const btn = document.querySelector(".btn");
+    const input = document.querySelector(".input");
+
+    input.value = "some text 1";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+    btn.click();
+
+    input.value = "some text 2";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+    btn.click();
+
+    input.value = "some text 3";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+    btn.click();
+
+    input.value = "some text 4";
+    input.dispatchEvent(new KeyboardEvent("keyup"));
+    btn.click();
+
+    expect(document.querySelectorAll(".text").length).toBe(5);
+    expect(document.querySelectorAll(".text")[0] === "no lorem");
+    expect(document.querySelectorAll(".text")[4] === "some text 4");
+  });
+});
